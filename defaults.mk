447a217c3476b4acc8a02896e48a8045551b814d
Christian Kroll <chris@das-labor.org>
2014-05-28 13:31:17 +0200

    preliminary NetBSD support (only tested on amd64 so far)

--- defaults.mk
+++ defaults.mk
@@ -40,7 +40,11 @@ ifeq ($(findstring CYGWIN,$(OSTYPE)),CYGWIN)
   ifeq ($(MACHINE),x86_64)
     LDFLAGS_SIM = -T ld_scripts/i386pep.x
   else
-    LDFLAGS_SIM = -T ld_scripts/i386pe.x
+    ifeq ($(MACHINE),i686)
+      LDFLAGS_SIM = -T ld_scripts/i386pe.x
+    else
+        $(error $(n)$(n)Simulator build is only supported on i386 and amd64.$(n)$(n))
+    endif
   endif
   LIBS_SIM    = -lgdi32 -lwinmm -lm
 else
@@ -50,24 +54,48 @@ else
     ifeq ($(MACHINE),amd64)
       LDFLAGS_SIM = -L/usr/local/lib -T ld_scripts/elf_x86_64_fbsd.x
     else
-      LDFLAGS_SIM = -L/usr/local/lib -T ld_scripts/elf_i386_fbsd.x
+      ifeq ($(MACHINE),i386)
+        LDFLAGS_SIM = -L/usr/local/lib -T ld_scripts/elf_i386_fbsd.x
+      else
+        $(error $(n)$(n)Simulator build is only supported on i386 and amd64.$(n)$(n))
+      endif
     endif
     LIBS_SIM = -lglut -lpthread -lGL -lGLU -lm
   else
-    ifeq ($(OSTYPE),Linux)
-      CFLAGS_SIM  = -g -Wall -pedantic -std=c99 -O0 -D_XOPEN_SOURCE=600
-      ifeq ($(MACHINE),x86_64)
-        LDFLAGS_SIM = -T ld_scripts/elf_x86_64.x
+    ifeq ($(OSTYPE),NetBSD)
+      CFLAGS_SIM = -g -I /usr/pkg/include -I/usr/X11R7/include -Wall -pedantic -std=c99 -O0
+      CFLAGS_SIM += -D_XOPEN_SOURCE=600
+      ifeq ($(MACHINE),amd64)
+        LDFLAGS_SIM = -L/usr/pkg/lib -L/usr/X11R7/lib -T ld_scripts/elf_x86_64_nbsd.x -Wl,-R/usr/pkg/lib,-R/usr/X11R7/lib
       else
-        LDFLAGS_SIM = -T ld_scripts/elf_i386.x
+        ifeq ($(MACHINE),i386)
+           LDFLAGS_SIM = -L/usr/pkg/lib -L/usr/X11R7/lib -T ld_scripts/elf_i386_nbsd.x -Wl,-R/usr/pkg/lib,-R/usr/X11R7/lib
+         else
+            $(error $(n)$(n)Simulator build is only supported on i386 and amd64.$(n)$(n))
+         endif
       endif
       LIBS_SIM = -lglut -lpthread -lGL -lGLU -lm
     else
-      ($(error $(n)$(n)Simulator build is not supported on your system.$(n)$(n)\
+      ifeq ($(OSTYPE),Linux)
+        CFLAGS_SIM  = -g -Wall -pedantic -std=c99 -O0 -D_XOPEN_SOURCE=600
+        ifeq ($(MACHINE),x86_64)
+          LDFLAGS_SIM = -T ld_scripts/elf_x86_64.x
+        else
+          ifeq ($(MACHINE),i686)
+            LDFLAGS_SIM = -T ld_scripts/elf_i386.x
+          else
+             $(error $(n)$(n)Simulator build is only supported on i386 and amd64.$(n)$(n))
+          endif
+        endif
+        LIBS_SIM = -lglut -lpthread -lGL -lGLU -lm
+      else
+        ($(error $(n)$(n)Simulator build is not supported on your system.$(n)$(n)\
 Currently supported platforms:$(n) \
   Linux on x86 and amd64$(n) \
   FreeBSD on x86 and amd64$(n) \
-  Windows (through Cygwin) on x86 and amd64)
+  NetBSD on x86 and amd64$(n) \
+  Windows (via Cygwin) on x86 and amd64)
+      endif
     endif
   endif
 endif
